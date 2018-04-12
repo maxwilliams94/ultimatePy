e6a36c5bf1714c18c3bef5eff3549d3eba3ca75b
Max Williams <max.williams@bristol.ac.uk>
2018-04-12 16:49:37 +0100

    development of initial methods and classes

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-import ultimatePy
+import ultimatepy as up
+
+list_of_teams = ['Bristol 1','Bath 2','Bristol 2','Bath 1']
+
+swsc = up.Tournament('South West Super Cup')
+
+swsc.import_teams(list_of_teams)
+
+print(swsc.team_list[0])
+
+for team_number in range(1,len(list_of_teams)+1):
+    team_number_str = str(team_number)
+    ob_name = "team"+team_number_str
+
+
+#swsc.get_team_list()
+
+director = up.ProcessDirector()
+
+#for index,name_of_team in enumerate(swsc.team_list):
+
+
+
+
 
--- ultimatepy.py
+++ ultimatepy.py
@@ -1,3 +1,53 @@
 import numpy as np
 from numpy import genfromtxt
 
+class ProcessDirector:
+    def __init__(self):
+        self.allClasses = []
+
+    def construct(self,className,builderName):
+        targetClass = getattr(str(builderName),className)
+        print(targetClass)
+
+class Tournament:
+    def __init__(self,name):
+        self.name = name
+        self.size = -1
+        self.structure = "null"
+        self.number_of_groups = 0
+        self.team_list = []
+
+    def import_teams(self,list):
+        self.team_list = list
+
+
+    def get_team_list(self):
+        for item in range(len(self.team_list)):
+            print(str(item+1)+ " " + str(self.team_list[item]))
+
+    #def create_teams(self):
+        #for index,name_of_team in enumerate(self.team_list):
+
+
+class Group:
+    def __init__(self):
+        self.identifier = "0"
+        self.size = 0
+        self.type = "-1"
+
+class Team:
+    def __init__(self,name):
+        self.name = name
+        self.seed = -1
+
+    def set_seed(self,seed):
+        self.seed = seed
+
+    def get_name(self):
+        print(self.name)
+
+    def get_seed(self):
+        print(self.seed)
+
+
+
