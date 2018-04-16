3cc28ff06bc50ac44897921abe9b52e245406623
Max Williams <max.williams@bristol.ac.uk>
2018-04-16 16:25:01 +0100

    setGroupSizes() creates groupDict, a tournament object which contains a list of the groups and their instances

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import ultimatepy as up
 
 # import list of teams
-list_of_teams = ['Bristol 1','Bath 2','Bristol 2','Bath 1']
+list_of_teams = ['Bristol 1','Bath 2','Bristol 2','Bath 1', 'Cardiff 1', 'Swansea 1', 'Bears 1', 'UWE 2']
 
 swsc = up.Tournament('South West Super Cup')
 
 
-swsc.import_teams(list_of_teams)
+swsc.importTeams(list_of_teams)
 
 teamDict = {}
 
-for teamNumber,team in enumerate(swsc.team_list):
+for teamNumber,team in enumerate(swsc.teamList):
     teamDict[str(team)] = up.Team(str(team))
+    teamDict[team].setSeed(teamNumber+1)
+
+swsc.setGroupSizes()
+
 
 
 
