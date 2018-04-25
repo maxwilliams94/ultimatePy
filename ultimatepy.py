730ad1454a9ddd072819984b8fe324cc16f1df94
Max Williams <max.williams@bristol.ac.uk>
2018-04-25 13:24:52 +0100

    remove str() from object dictionary calling and creation add method for creating teamList within Group

--- ultimatepy.py
+++ ultimatepy.py
@@ -22,9 +22,9 @@ class Tournament:
         self.size = len(self.teamList)
         # create team objects
         for teamNumber, team in enumerate(self.teamList):
-            self.teamDict[str(team)] = Team(str(team))
+            self.teamDict[team] = Team(team)
             self.teamDict[team].setSeed(teamNumber + 1)
-            self.seedDict[str(teamNumber+1)] = Team(str(Team))
+            self.seedDict[teamNumber+1] = Team(Team)
             self.seedList.append(teamNumber+1)
         for groupSize in range(1,10):
             if (self.size/groupSize) % 2 == 0:
@@ -46,21 +46,25 @@ class Tournament:
 
         if self.numberOfGroups != 0:
             for code in range(65,65+int(self.numberOfGroups)):
-                self.groupDict[str(chr(code))] = Group(chr(code),int(self.size)/int(self.numberOfGroups))
-                self.groupList.append(str(chr(code)))
+                self.groupDict[chr(code)] = Group(chr(code),int(self.size)/int(self.numberOfGroups))
+                self.groupList.append(chr(code))
 
     def assignGroupSeeds(self):
         # assign team seeds into group seedLists
         groupSize = self.size/len(self.groupList)
         for iteration in range(int(groupSize/2)):
             for group in self.groupDict.keys():
-                self.groupDict[str(group)].appendSeed(self.seedList.pop(0))
-                self.groupDict[str(group)].appendSeed(self.seedList.pop(-1))
-                print("{}: {} ".format(self.groupDict[str(group)],self.groupDict[str(group)].getTeamList))
+                self.groupDict[group].appendSeed(self.seedList.pop(0))
+                self.groupDict[group].appendSeed(self.seedList.pop(-1))
+
+        for group in self.groupDict:
+            for seed in self.groupDict[group].seedList:
+                teamName = self.teamList[seed-1]
+                self.groupDict[group].teamList.append(teamName)
+                if len(self.groupDict[group].teamList) == self.groupDict[group].size:
+                    print("Group {}: {}".format(self.groupDict[group].identifier,self.groupDict[group].teamList))
 
 
-
-        # these will then be used to assign objects to the seedDict
 
     def getTeamList(self):
         for item in range(len(self.teamList)):
@@ -74,15 +78,16 @@ class Group:
         self.size = size
         self.type = "-1"
         self.teamList = []
+        self.seedList = []
 
     def appendSeed(self,seed):
-        self.teamList.append(seed)
-        self.teamList.sort()
-        print(self.teamList)
+        self.seedList.append(seed)
+        self.seedList.sort()
 
     def getTeamList(self):
-        return self.teamList
-
+        print("Group {}".format(self.identifier))
+        for item in self.teamList:
+            print(item)
     def __repr__(self):
         return "Group {}".format(self.identifier)
 
@@ -111,9 +116,14 @@ swsc = Tournament('South West Super Cup')
 
 swsc.importTeams(['Bristol 1','Bath 2','Bristol 2','Bath 1', 'Cardiff 1', 'Swansea 1', 'Bears 1', 'UWE 2'])
 
+# swsc.getTeamList()
+
 swsc.setGroupSizes()
 
 swsc.assignGroupSeeds()
+
+
+
 
 
 
