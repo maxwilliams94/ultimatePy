5813f90b7dd5b8d46a897fec982bd4b871b95bcf
Max Williams <max.williams@bristol.ac.uk>
2018-09-18 18:28:35 +0100

    create Group class with methods for adding a team to its team list and a self calculating the size of the group

--- ultimatepy.py
+++ ultimatepy.py
@@ -18,5 +18,16 @@ class Team:
     def __repr__(self):
         return '{}: {}'.format(self.seed,self.name)
 
+class Group:
+    def __init__(self, identifier):
+        self.id = identifier
+        self.team_list = []
+        self.size = 0
+    def addTeam(self,team_name):
+        self.team_list.append(str(team_name))
+
+    def setSize(self):
+        self.size = len(self.team_list)
+
 
 # if __name__ == '__main__':
