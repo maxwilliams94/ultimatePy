46b9f23ad5157fdba08ede7fe912f6abb1f41515
Max Williams <max.williams@bristol.ac.uk>
2018-09-18 18:55:31 +0100

    creation of groupDict according to number of groups calculated

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from ultimatepy import Team
+from ultimatepy import Team,Group
 
 # import team list
 teamList = ["Uni A","Uni B", "Uni C", "Uni D", "Uni E", "Uni F", "Uni G", "Uni H"]
 # count number of teams
 numTeams = len(teamList)
 # get number of groups
-numGroups = len(teamList)/4
+numGroups = int(len(teamList)/4)
 print("{} Groups of 4 Teams".format(numGroups))
 
 # Populate TeamDict with Team objects
@@ -14,6 +14,14 @@ for teamNumber,team in enumerate(teamList):
     teamDict[str(team)] = Team(str(team),teamNumber+1)
 
 
+# Create dictionary of group objects
+groupDict = {}
+groupList = []
+for number in range(0,numGroups):
+    group_name = chr(65 + number)
+    groupDict[group_name] = Group(group_name)
+    groupList.append(group_name)
 
-
-
+# Create team_list dummy
+teamListDummy = teamList
+# Iterate through the team list and assign each team to a group according to seed
