26f65304ab918fb56d262fb43975781b0d2a7901
Max Williams <mwi2004@gmail.com>
2018-09-22 15:40:32 +0100

    move to snake case from CamelCase

--- main.py
+++ main.py
@@ -3,35 +3,35 @@ from ultimatepy import Team,Group
 # import team list
 all_teams = ["Uni A","Uni B", "Uni C", "Uni D", "Uni E", "Uni F", "Uni G", "Uni H"]
 # count number of teams
-numTeams = len(all_teams)
+total_teams = len(all_teams)
 # get number of groups
-numGroups = int(len(all_teams)/4)
-print("{} Groups of 4 Teams".format(numGroups))
+total_groups = len(all_teams) // 4
+print("{} Groups of 4 Teams".format(total_groups))
 
 # Populate TeamDict with Team objects
-teamDict = {}
-for teamNumber,team in enumerate(all_teams):
-    teamDict[str(team)] = Team(str(team),teamNumber+1)
+team_dict = {}
+for team_num, team in enumerate(all_teams):
+    team_dict[str(team)] = Team(str(team), team_num + 1)
 
 
 # Create dictionary of group objects
-groupDict = {}
-groupList = []
-for number in range(0,numGroups):
+group_dict = {}
+group_list = []
+for number in range(0, total_groups):
     group_name = chr(65 + number)
-    groupDict[group_name] = Group(group_name)
-    groupList.append(group_name)
+    group_dict[group_name] = Group(group_name)
+    group_list.append(group_name)
 
 # Create team_list dummy
-teamListDummy = all_teams
+team_list_cp = all_teams
 # Iterate through the team list and assign each team to a group according to seed
 for num in range(int(len(all_teams)/2)):
-    dest_group = groupList[num%len(groupList)]
-    groupDict[dest_group].addTeam(teamListDummy.pop(0))
-    groupDict[dest_group].addTeam(teamListDummy.pop(-1))
-    groupDict[dest_group].team_list.sort()
+    dest_group = group_list[num % len(group_list)]
+    group_dict[dest_group].addTeam(team_list_cp.pop(0))
+    group_dict[dest_group].addTeam(team_list_cp.pop(-1))
+    group_dict[dest_group].team_list.sort()
 
-for group in groupList:
+for group in group_list:
     print(group)
-    for team in groupDict[group].team_list:
-        print(teamDict[team])
+    for team in group_dict[group].team_list:
+        print(team_dict[team])
