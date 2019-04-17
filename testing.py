547063ff6ae1bf4a9bf7abf48707d7d1fa11be7c
Max Williams <max.williams@bristol.ac.uk>
2019-04-17 18:59:12 +0100

    update timings definition

--- testing.py
+++ testing.py
@@ -6,16 +6,19 @@ from datetime import datetime, timedelta
 team_input = ["Uni 1", "Uni 2", "Uni 3", "Uni 4", "Uni 5", "Uni 6", "Uni 7", "Uni 8"]
 # Tournament timings stored within namedTuple
 # Game/Day Variables
-timings = Timings(game_length=timedelta(hours=1),
+timings = Timings(group_game_length=timedelta(minutes=50),
+                  game_length=timedelta(hours=1),
                   game_break=timedelta(minutes=5),
-                  day_start=datetime(year=1, month=1, day=1, hour=9),
-                  day_length=timedelta(hours=8))
+                  day1_start=datetime(year=1, month=1, day=1, hour=9),
+                  day2_start=datetime(year=1, month=1, day=1, hour=9),
+                  day1_length=timedelta(hours=8),
+                  day2_length=timedelta(hours=8))
 
 t = Tournament(team_list=team_input)
 t.create_groups(group_size=4)
 t.create_pitches(total_pitches=1)
 
-assert t.tot_teams == len(team_input), "total teams matches length of input team list"
+assert t.total_teams == len(team_input), "total teams matches length of input team list"
 assert len(t.groups) == len(team_input)//t.group_size, "correct number of groups created"
 
 for group in t.groups.values():
